97035df8944a0dcc568f945b533a1ae2c8dcffd6
Pantawan Vongvattanakun (Pall) <pantawan@live.dk>
2026-03-24 18:38:36 +0100

    Refactor App.vue to implement page navigation with Home, MyItems, and AddDetails components

--- semester_project/src/App.vue
+++ semester_project/src/App.vue
@@ -1,8 +1,66 @@
+<script>
+import Home from "@/components/Home.vue";
+import MyItems from "@/components/MyItems.vue";
+import AddDetails from "@/components/AddDetails.vue";
+
+export default {
+  components: {
+    Home,
+    MyItems,
+    AddDetails,
+  },
+  data() {
+    return {
+      currentPage: "home",
+      itemDetails: null,
+    };
+  },
+  methods: {
+    goHome() {
+      this.currentPage = "home";
+    },
+    goToItems() {
+      this.currentPage = "items";
+    },
+    goToAddDetails() {
+      this.currentPage = "addDetails";
+    },
+    handleSaveDetails(details) {
+      this.itemDetails = details;
+      console.log("Saved:", details);
+      this.currentPage = "items"; //Change to next page when implemented
+    },
+  },
+};
+</script>
+
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <!-- Page navigation -->
+      <Home v-if="currentPage === 'home'" @go-to-items="goToItems" />
+
+      <MyItems
+        v-if="currentPage === 'items'"
+        @go-to-home="goHome"
+        @new-item="goToAddDetails"
+      />
+
+      <AddDetails
+        v-if="currentPage === 'addDetails'"
+        @go-to-home="goHome"
+        @save-details="handleSaveDetails"
+      />
     </v-main>
+
+    <v-btn
+      class="ma-2"
+      icon="mdi-home"
+      location="top left"
+      position="absolute"
+      @click="goHome"
+    />
+
     <v-btn
       class="ma-2"
       icon="mdi-theme-light-dark"
@@ -12,7 +70,3 @@
     />
   </v-app>
 </template>
-
-<script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
