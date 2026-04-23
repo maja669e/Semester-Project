02dfe2965a03a656837e0adef7ffe3770a1be13d
maja669e <maja.bijedic@live.dk>
2026-04-23 16:54:02 +0200

    Database connection works locally

---
+++ semester_project/server/server.js
@@ -0,0 +1,19 @@
+const db = require("./models");
+
+// Test DB connection first
+db.sequelize.authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// Then sync models
+db.sequelize.sync({ alter: true })
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
